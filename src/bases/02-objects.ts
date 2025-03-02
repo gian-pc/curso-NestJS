83ae4fea29953663f6ea172b7903a4e679adf5f1
gianpc <gpaucarcortez@gmail.com>
2025-03-02 16:44:48 -0500

    feat: añade conversión rápida de string a número en pokemonIds

--- src/bases/02-objects.ts
+++ src/bases/02-objects.ts
@@ -1,4 +1,6 @@
 export const pokemonIds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-pokemonIds.push('Hola'); // ❌ Error: 'Hola' no es un número
+pokemonIds.push(+'11'); // ✅ Una forma rápida de conversion de tipos
+
+console.log(pokemonIds); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 
