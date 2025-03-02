3c1fb26d9a1ddc3d45d1fff18a58561d668d7931
gianpc <gpaucarcortez@gmail.com>
2025-03-02 16:04:09 -0500

    feat: añade uso de template strings para manejar cadenas de texto multilinea con interpolación

--- src/bases/01-types.ts
+++ src/bases/01-types.ts
@@ -3,3 +3,16 @@ export const age = 24;      // Constante, no puede cambiar
 export const isActive = true; 
 
 name = 'gian@pc'; // ✅ Ahora esto es válido
+
+export const templateString = `Esto es un string
+multilinea
+que puede tener
+"comillas dobles"
+y 'comillas simples'
+inyectar valores ${name}
+expresiones ${age + 1}
+numeros: ${5 + 5}
+booleanos: ${isActive}
+`;
+
+console.log(templateString); 
