cac13c9621c33bd05a4dfb5a58b92228922523bf
gianpc <gpaucarcortez@gmail.com>
2025-03-10 20:20:03 -0500

    feat: agregamos readonly al id para que no se pueda modificar

--- src/bases/03-classes.ts
+++ src/bases/03-classes.ts
@@ -3,9 +3,12 @@
 export class Pokemon {
 
     constructor(
-        public id: number,
+        public readonly id: number,
         public name: string
     ) {}
 }
 
 export const charmander = new Pokemon(1, 'Charmander');
+
+charmander.id  = 10; // ❌ No se puede reasignar el valor de una propiedad de solo lectura
+charmander.name = 'Mew'; // ✅ Se puede reasignar el valor de una propiedad publica
