74e8451a409503ceb7a6f6825033a622330a6522
gianpc <gpaucarcortez@gmail.com>
2025-03-10 20:17:52 -0500

    feat: forma corta de definir una clase y es la más común

--- src/bases/03-classes.ts
+++ src/bases/03-classes.ts
@@ -1,15 +1,11 @@
 
-// Forma larga de declarar una clase
-export class Pokemon{
-    public id: number;
-    public name: string;
-   
-    constructor(id: number, name: string){
-        this.id = id;
-        this.name = name;
-        console.log('constructor llamado')
-    }
+// Forma corta de declarar una clase
+export class Pokemon {
 
+    constructor(
+        public id: number,
+        public name: string
+    ) {}
 }
 
 export const charmander = new Pokemon(1, 'Charmander');
